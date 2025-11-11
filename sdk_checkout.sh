43c67392c652b15ec87a2c0c00b331093f8fc058
Dollar <dollar@acsgconsults.com>
2025-11-11 10:51:21 +0800

    Switch to SDK11.00.09.04

--- sdk_checkout.sh
+++ sdk_checkout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WORKDIR="SDK11"
 git clone https://git.ti.com/git/arago-project/oe-layersetup.git ${WORKDIR} && cd ${WORKDIR}
-./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-scarthgap-chromium-11.01.16.13-config.txt
+./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-scarthgap-chromium-11.00.09.04-config.txt
 
 
